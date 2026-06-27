951fc8875159d263838737aea9426eba635ff1c4
daniel-000com <danielorrala5552@gmail.com>
2026-06-27 18:26:59 -0500

    Add food and use the iniciarJuego function.

--- cazando.js
+++ cazando.js
@@ -2,13 +2,28 @@ let canvas;
 let ctx;
 canvas=document.getElementById("areaJuego");
 ctx=canvas.getContext("2d");
-const ALTURA_GATO =40;
-const ANCHO_GATO =40;
-let gatoX =canvas.width/2-20
-let gatoY =canvas.height/2-20
+const ALTURA_GATO =80;
+const ANCHO_GATO =80;
+const ALTURA_COMIDA =60;
+const ANCHO_COMIDA =60;
+let gatoX =canvas.width/2-40
+let gatoY =canvas.height/2-40
+let comidaX = 0
+let comidaY = 0
+
+function iniciarJuego(){
+    graficarGato();
+    graficarComida();
+}
 
 function graficarGato(){
     ctx.fillStyle="#F5A327";
     ctx.fillRect(gatoX, gatoY, ALTURA_GATO, ANCHO_GATO)
+
+}
+
+function graficarComida(){
+    ctx.fillStyle="#905049";
+    ctx.fillRect(comidaX, comidaY, ALTURA_COMIDA, ANCHO_COMIDA)
 
 }
